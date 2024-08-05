c7bef75c8800ee955e197be3d5ecfa8e0ee7fdc0
JordanBandur <jordanbandur@hotmail.ca>
2024-08-04 23:44:34 -0600

    Added rotation logic

--- PoolCue.js
+++ PoolCue.js
@@ -3,12 +3,23 @@ const STICK_ORIGIN = new Vector2(970, 11);
 function PoolCue(position) {
 
   this.position = position;
+  this.rotation = 0;
 }
 
 PoolCue.prototype.update = function () {
 
+  this.updateRotation();
 };
 
 PoolCue.prototype.draw = function () {
   Canvas.drawImage(sprites.poolCue, this.position, STICK_ORIGIN);
+};
+
+PoolCue.prototype.updateRotation = function () {
+  // Used the information from this article to find the formula for calculating the rotation:
+  // https://sinepost.wordpress.com/2012/02/16/theyve-got-atan-you-want-atan2/
+  let opposite = Mouse.position.y - this.position.y;
+  let adacent = Mouse.position.x - this.position.x;
+
+  this.rotation = Math.atan2(opposite, adacent);
 };
