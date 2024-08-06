8acb7031a6a2d5cfc6bfb280321dba60af2c096f
JordanBandur <jordanbandur@hotmail.ca>
2024-08-06 11:26:10 -0600

    Add logic to more accurately calculate vector

--- Ball.js
+++ Ball.js
@@ -2,10 +2,11 @@ const BALL_ORIGIN = new Vector2(25, 25);
 
 function Ball(position) {
   this.position = position;
+  this.velocity = new Vector2();
 }
 
-Ball.prototype.update = function () {
-
+Ball.prototype.update = function (delta) {
+  this.position.addTo(this.velocity.mult(delta));
 };
 
 Ball.prototype.draw = function () {
@@ -15,5 +16,5 @@ Ball.prototype.draw = function () {
 
 Ball.prototype.shoot = function (power, rotation) {
 
-  console.log("shoot");
+  this.velocity = new Vector2(power * Math.cos(rotation), power * Math.sign(rotation));
 };
