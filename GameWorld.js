20ededae6a91c3ba9cb4acfa4e38f7d96e36f0f0
JordanBandur <jordanbandur@hotmail.ca>
2024-08-06 11:27:08 -0600

    Update logic for physics

--- GameWorld.js
+++ GameWorld.js
@@ -1,16 +1,19 @@
 // This is resonsible for a the all the 'physical' items in the game. For each frame of animation
 // this will be responsible to update the items and draw them on the canvas.
+const DELTA = 1 / 100; // The amount to update on each iteration
 
 function GameWorld() {
 
   this.poolCue = new PoolCue(new Vector2(413, 413));
-  this.cueBall = new Ball(new Vector2(413, 413), this.cueBall.shoot);
+  this.cueBall = new Ball(
+    new Vector2(413, 413),
+    this.cueBall.shoot.bind(this.cueBall));
 }
 
 GameWorld.prototype.update = function () {
 
   this.poolCue.update();
-  this.cueBall.update();
+  this.cueBall.update(DELTA);
 };
 
 GameWorld.prototype.draw = function () {
