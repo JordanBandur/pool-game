3435537c55f905a822e07e8c24ac77a5d69d49b2
JordanBandur <jordanbandur@hotmail.ca>
2024-08-06 23:01:43 -0600

    Add method to calculate the dot product of vectors

--- Vector2.js
+++ Vector2.js
@@ -27,6 +27,10 @@ Vector2.prototype.mult = function (scalar) {
   return new Vector2(this.x * scalar, this.y * scalar);
 };
 
+Vector2.prototype.dot = function(vector) {
+  return this.x * vector.x + this.y * vector.y;
+}
+
 Vector2.prototype.length = function () {
 
   return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
