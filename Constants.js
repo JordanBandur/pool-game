56884cb1af635da5a95cad1746ed9b43fc33a712
JordanBandur <jordanbandur@hotmail.ca>
2024-08-10 21:50:39 -0600

    Make seperate file to hold constants

---
+++ Constants.js
@@ -0,0 +1,18 @@
+const CONSTANTS = {
+  ballParams: [
+    new Ball(new Vector2(1056, 433), COLOR.RED),//3
+    new Ball(new Vector2(1090, 374), COLOR.RED),//4
+    new Ball(new Vector2(1126, 393), COLOR.RED),//8
+    new Ball(new Vector2(1126, 472), COLOR.RED),//10;
+    new Ball(new Vector2(1162, 335), COLOR.RED),//11
+    new Ball(new Vector2(1162, 374), COLOR.RED),//12
+    new Ball(new Vector2(1162, 452), COLOR.RED),//14
+    new Ball(new Vector2(1022, 413), COLOR.YELLOW),//1
+    new Ball(new Vector2(1056, 393), COLOR.YELLOW),//2
+    new Ball(new Vector2(1090, 452), COLOR.YELLOW),//6
+    new Ball(new Vector2(1126, 354), COLOR.YELLOW),//7
+    new Ball(new Vector2(1126, 433), COLOR.YELLOW),//9
+    new Ball(new Vector2(1162, 413), COLOR.YELLOW),//13
+    new Ball(new Vector2(1162, 491), COLOR.YELLOW)//15
+  ]
+};
