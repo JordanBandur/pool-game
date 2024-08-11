d91a92a882c6aecc933452ae8d11c997140f81e0
JordanBandur <jordanbandur@hotmail.ca>
2024-08-10 21:50:59 -0600

    Refactor code to seperate constants from logic

--- GameWorld.js
+++ GameWorld.js
@@ -5,24 +5,9 @@ const DELTA = 1 / 178; // The amount to update on each iteration
 
 function GameWorld() {
 
-  this.poolBalls = [
-    new Ball(new Vector2(1056, 433), COLOR.RED),//3
-    new Ball(new Vector2(1090, 374), COLOR.RED),//4
-    new Ball(new Vector2(1126, 393), COLOR.RED),//8
-    new Ball(new Vector2(1126, 472), COLOR.RED),//10;
-    new Ball(new Vector2(1162, 335), COLOR.RED),//11
-    new Ball(new Vector2(1162, 374), COLOR.RED),//12
-    new Ball(new Vector2(1162, 452), COLOR.RED),//14
-    new Ball(new Vector2(1022, 413), COLOR.YELLOW),//1
-    new Ball(new Vector2(1056, 393), COLOR.YELLOW),//2
-    new Ball(new Vector2(1090, 452), COLOR.YELLOW),//6
-    new Ball(new Vector2(1126, 354), COLOR.YELLOW),//7
-    new Ball(new Vector2(1126, 433), COLOR.YELLOW),//9
-    new Ball(new Vector2(1162, 413), COLOR.YELLOW),//13
-    new Ball(new Vector2(1162, 491), COLOR.YELLOW)//15
-  ].map(params => new Ball(params[0], params[1]));
+  this.poolBalls = CONSTANTS.ballParams.map(params => new Ball(...params));
 
-  this.cueBall = this.poolBalls[this.poolBalls.length - 1];
+  this.cueBall = this.poolBalls.find(ball => ball.color === COLOR.WHITE);
   this.poolCue = new PoolCue(
     new Vector2(413, 413),
     this.poolCue.shoot.bind(this.cueBall));
